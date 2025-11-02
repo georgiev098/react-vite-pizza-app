d43c4d85613f5e8aa1b2a11e51b7a31f6f6c4d68
Daniel Georgiev <georgiev.daniel@outlook.com>
2025-11-02 17:23:37 +0200

    (feat) implement basic app layout

---
+++ src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <div>
+      <header>
+        <Link to="/">Mamma mia.</Link>
+        <p>daniel</p>
+      </header>
+    </div>
+  );
+}
