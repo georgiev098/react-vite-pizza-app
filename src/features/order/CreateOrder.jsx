21bf1c4d9970989c711b5bc818568199d5579b1e
georgiev098 <georgiev.daniel@outlook.com>
2025-11-06 16:19:02 +0200

    (style) style the order page

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -40,31 +40,40 @@ export default function CreateOrder() {
   const formErrors = useActionData();
 
   return (
-    <div>
-      <h2>Ready to order? Let's go!</h2>
+    <div className="py-6 px-4">
+      <h2 className="text-xl font-semibold mb-8">Ready to order? Let's go!</h2>
 
       <Form method="POST">
-        <div>
-          <label>First Name</label>
-          <input type="text" name="customer" required className="input" />
+        <div className="mb-5 flex gap-2 flex-col sm:flex-row sm:items-center ">
+          <label className="sm:basis-40">First Name</label>
+          <input type="text" name="customer" required className="input grow" />
         </div>
 
-        <div>
-          <label>Phone number</label>
-          <div>
-            <input type="tel" name="phone" required className="input" />
-          </div>
-          {formErrors?.phone && <p>{formErrors.phone}</p>}
-        </div>
-
-        <div>
-          <label>Address</label>
-          <div>
-            <input type="text" name="address" required className="input" />
+        <div className="mb-5 flex gap-2 flex-col sm:flex-row sm:items-center ">
+          <label lassName="sm:basis-40">Phone number</label>
+          <div className="grow">
+            <input type="tel" name="phone" required className="input w-full" />
+            {formErrors?.phone && (
+              <p className="text-xs mt-2 text-red-700 bg-red-100 p-2 rounded-md">
+                {formErrors.phone}
+              </p>
+            )}
           </div>
         </div>
 
-        <div>
+        <div className="mb-5 flex gap-2 flex-col sm:flex-row sm:items-center ">
+          <label lassName="sm:basis-40">Address</label>
+          <div className="grow ">
+            <input
+              type="text"
+              name="address"
+              required
+              className="input w-full"
+            />
+          </div>
+        </div>
+
+        <div className="mb-12 flex gap items-center font-medium">
           <input
             className="h-6 w-6 accent-amber-400 focus:outline-none focus:ring focus:ring-amber-400 focus:ring-offset-2"
             type="checkbox"
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -2,10 +2,12 @@ import { Link } from "react-router-dom";
 
 export default function Button({ children, disabled, to, type }) {
   const base =
-    "bg-amber-400 uppercase font-semibold text-stone-800 inline-block tracking-wide rounded-full hover:bg-amber-300 transition-colors focus:outline-none focus:ring focus:ring-amber-300 focus:ring-offset-2 disabled:cursor-not-allowed";
+    "bg-amber-400 text-sm uppercase font-semibold text-stone-800 inline-block tracking-wide rounded-full hover:bg-amber-300 transition-colors focus:outline-none focus:ring focus:ring-amber-300 focus:ring-offset-2 disabled:cursor-not-allowed";
 
   const styles = {
     primary: base + " py-3 px-4 md:px-6 md:py-4",
+    secondary:
+      "uppercase text-sm border-2 border-stone-300 py-4 px-2.5 md:px-6 md:py-3.5 font-semibold text-stone-400 inline-block tracking-wide rounded-full hover:bg-stone-500 transition-colors focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed",
     small: base + " px-4 py-2 md:px-5 md:py-2.5 text-xs",
   };
 
